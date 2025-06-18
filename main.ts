47d215a90f74687684b20c05555146d7badab7ad
Kora Reinolds <s.shurin94@gmail.com>
2025-06-18 21:10:17 +0300

    init mcp

--- main.ts
+++ main.ts
@@ -1,85 +1,38 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import * as http from 'http';
+import { AddressInfo } from 'net';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface KoraMcpPluginSettings {
+	port: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: KoraMcpPluginSettings = {
+	port: 8123,
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class KoraMcpPlugin extends Plugin {
+	settings: KoraMcpPluginSettings;
+	private server: http.Server | null = null;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addSettingTab(new McpSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.startServer();
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'restart-mcp-server',
+			name: 'Restart MCP Server',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
+				this.stopServer();
+				this.startServer();
+			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
+		this.stopServer();
 	}
 
 	async loadSettings() {
@@ -89,46 +42,104 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	startServer() {
+		if (this.server) {
+			new Notice('MCP server is already running.');
+			return;
+		}
+
+		this.server = http.createServer(async (req, res) => {
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+			res.setHeader(
+				'Access-Control-Allow-Headers',
+				'Content-Type, Authorization'
+			);
+
+			if (req.method === 'OPTIONS') {
+				res.writeHead(204);
+				res.end();
+				return;
+			}
+
+			if (req.url === '/files' && req.method === 'GET') {
+				const files = this.app.vault.getMarkdownFiles().map((f) => ({
+					path: f.path,
+					basename: f.basename,
+					stat: f.stat,
+				}));
+				res.writeHead(200, { 'Content-Type': 'application/json' });
+				res.end(JSON.stringify(files));
+				return;
+			}
+
+			res.writeHead(404, { 'Content-Type': 'application/json' });
+			res.end(JSON.stringify({ error: 'Not Found' }));
+		});
+
+		this.server.on('error', (err: NodeJS.ErrnoException) => {
+			if (err.code === 'EADDRINUSE') {
+				new Notice(
+					`MCP Server: Port ${this.settings.port} is already in use. Please change the port in the settings.`
+				);
+				this.server = null;
+			} else {
+				new Notice(`MCP Server error: ${err.message}`);
+			}
+		});
+
+		this.server.listen(this.settings.port, '127.0.0.1', () => {
+			const address = this.server?.address() as AddressInfo;
+			new Notice(`MCP server started on port ${address.port}`);
+			console.log(
+				`MCP server listening on http://127.0.0.1:${address.port}`
+			);
+		});
+	}
+
+	stopServer() {
+		if (this.server) {
+			this.server.close(() => {
+				new Notice('MCP server stopped.');
+				console.log('MCP server stopped.');
+				this.server = null;
+			});
+		}
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class McpSettingTab extends PluginSettingTab {
+	plugin: KoraMcpPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: KoraMcpPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'MCP Server Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Server Port')
+			.setDesc(
+				'The port for the MCP server to listen on. The server needs to be restarted for changes to take effect.'
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder('Enter port')
+					.setValue(this.plugin.settings.port.toString())
+					.onChange(async (value) => {
+						const port = parseInt(value, 10);
+						if (!isNaN(port) && port > 0 && port < 65536) {
+							this.plugin.settings.port = port;
+							await this.plugin.saveSettings();
+						}
+					})
+			);
 	}
 }
